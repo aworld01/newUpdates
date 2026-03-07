f277402575c35258fefb64bd1c8185f3d92a6fba
Termux <abdulzoha786@gmail.com>
2026-03-07 16:33:45 +0300

    7th commit

---
+++ amp_calculator.py
@@ -0,0 +1,19 @@
+import math
+V = int(input("Enter the voltage: "))
+W = int(input("Enter the watts: "))
+
+def electricity(v,w):
+    A = w/v
+    ohm = v/A
+    ohm =  f"{ohm:.2f}"
+    I = f"{A:.2f}"
+    VA = w/.8
+    print()
+
+    print(f"Voltage: {v} V")
+    print(f"Watt: {w} W")
+    print(f"VA: {VA} VA")
+    print(f"Current: {I} A")
+    print(f"Resistence: {ohm} Ohm")
+    
+electricity(V,W)
